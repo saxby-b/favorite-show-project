6b2e9d18fbeeb2826160b7c13fd5a31caed1033a
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-18 15:48:32 -0500

    Deleted comments and formatted code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-// add button
 const addShowButton = document.querySelector(".add");
-// label for the add button
 const showInput = document.querySelector(".add-show input");
-// unordered list (not yet visible)
 const showList = document.querySelector(".show-list");
-// span class for number of favorite shows
 const showCount = document.querySelector(".number");
 
 addShowButton.addEventListener("click", function () {
